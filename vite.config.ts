19cd195c66241d2ae39cc7691481da4978e508ed
r77tchan <r77tchan@gmail.com>
2025-08-03 17:51:05 +0900

    gh-pagesインストール

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/qr-code-component-main/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
